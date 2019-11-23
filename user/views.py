26a1821aaf754cccd4f6a79a2a6f8246cb01646a
jdineen21 <jdineen21@gmail.com>
2019-11-23 20:13:54 +0000

    signup page improvements

--- user/views.py
+++ user/views.py
@@ -3,10 +3,7 @@ from django.shortcuts import render
 from .forms import SignupForm
 
 def signup(request):
-    context = {
-        'signup_form': SignupForm(auto_id=False)
-    }
-    return render(request, 'user/signup.html', context)
+    return render(request, 'user/signup.html')
 
 def login(request):
     return render(request, 'user/login.html')
