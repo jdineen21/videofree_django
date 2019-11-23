d6eb2362bd9aea2527e67677fb8d5c69c6300b7e
jdineen21 <jdineen21@gmail.com>
2019-11-23 15:28:04 +0000

    basic footage model created

--- footage/models.py
+++ footage/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Footage(models.Model):
+    OTHER_TAG = 'OTH'
+    FOOTAGE_TAGS = [
+        ('ACT', 'Action Stock'),
+        ('DRN', 'Drone Stock'),
+        ('LND', 'Landscapes Stock'),
+        (OTHER_TAG, 'Other'),
+    ]
+    title = models.CharField(max_length=50)
+    upload_datetime = models.DateTimeField()
+    tags = models.CharField(
+        max_length=3,
+        choices=FOOTAGE_TAGS,
+        default=OTHER_TAG,
+    )
+    description = models.TextField()
