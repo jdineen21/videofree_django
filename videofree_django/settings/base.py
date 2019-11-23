0159a6156eb69fb6918e8230211d4e53c2e0a025
jdineen21 <jdineen21@gmail.com>
2019-11-23 14:19:30 +0000

    added static and media folder path to settings

--- videofree_django/settings/base.py
+++ videofree_django/settings/base.py
@@ -97,3 +97,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'sfiles')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+# Media files where uploaded and downloadable content are stored
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+ADMIN_MEDIA_PREFIX = '/media/admin/'
