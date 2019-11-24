ec1c33a39369e91d7bbe9605f72adf4980028cb7
jdineen21 <jdineen21@gmail.com>
2019-11-24 16:59:11 +0000

    form backend linked up

--- user/forms.py
+++ user/forms.py
@@ -1,9 +1,13 @@
+
 from django import forms
+
+ALPHA = 'abcdefghijklmnopqrstuvwxyz'
 
 class SignupForm(forms.Form):
     username = forms.CharField(
         label='',
-        max_length=20,
+        max_length=30,
+        required=False,
         widget=forms.TextInput(attrs={
             'class': 'text_input',
             'placeholder': 'Username',
@@ -12,22 +16,25 @@ class SignupForm(forms.Form):
     first_name = forms.CharField(
         label='',
         max_length=20,
+        required=False,
         widget=forms.TextInput(attrs={
             'class': 'text_input',
             'placeholder': 'First name',
         })
     )
-    second_name = forms.CharField(
+    last_name = forms.CharField(
         label='',
-        max_length=20,
+        max_length=30,
+        required=False,
         widget=forms.TextInput(attrs={
             'class': 'text_input',
-            'placeholder': 'Second name',
+            'placeholder': 'Surname',
         })
     )
     email = forms.CharField(
         label='',
-        max_length=20,
+        max_length=100,
+        required=False,
         widget=forms.EmailInput(attrs={
             'class': 'text_input',
             'placeholder': 'Email address',
@@ -35,9 +42,71 @@ class SignupForm(forms.Form):
     )
     password = forms.CharField(
         label='',
-        max_length=20,
+        max_length=50,
+        required=False,
         widget=forms.PasswordInput(attrs={
             'class': 'text_input',
             'placeholder': 'Password',
         })
-    ) 
+    )
+
+    def clean_username(self):
+        # Add symbol validation
+        username = self.cleaned_data.get('username')
+        if len(username) == 0:
+            raise forms.ValidationError('This field is required')
+        if len(username) < 5:
+            raise forms.ValidationError('This username is too short')
+        if len(username) > 30:
+            raise forms.ValidationError('This username is too long')
+        return username
+
+    def clean_first_name(self):
+        first_name = self.cleaned_data.get('first_name')
+        if len(first_name) == 0:
+            raise forms.ValidationError('This field is required')
+        if not first_name.isalpha():
+            raise forms.ValidationError('Only letters are allowed in names')
+        return first_name[0].upper() + first_name[1:]
+    
+    def clean_last_name(self):
+        last_name = self.cleaned_data.get('last_name')
+        if len(last_name) == 0:
+            raise forms.ValidationError('This field is required')
+        if len(last_name) < 2:
+            raise forms.ValidationError('This field must be at least 2 characters in length')
+        if not last_name.isalpha():
+            raise forms.ValidationError('Only letters are allowed in names')
+        return last_name[0].upper() + last_name[1:]
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if len(email) == 0:
+            raise forms.ValidationError('This field is required')
+        if not email.count('@') == 1:
+            raise forms.ValidationError('Email must contain exactly 1 @ symbol')
+
+        recipient_name = email[:email.find('@')]
+        if len(recipient_name) == 0:
+            raise forms.ValidationError('Email must be in format example@example.com')
+        if len(recipient_name) > 64:
+            raise forms.ValidationError('The recipient name may be a maximum of 64 characters long')
+        
+        # Domain length check
+        # Domain symbol check
+        full_domain = email[email.find('@')+1:]
+        domain_levels = full_domain.split('.')
+        for domain in domain_levels:
+            if len(domain) == 0:
+                raise forms.ValidationError('This email is not valid')
+
+        return email
+    
+    def clean_password(self):
+        # Password complexity check needs adding max legnth as well
+        password = self.cleaned_data.get('password')
+        if len(password) == 0:
+            raise forms.ValidationError('This field is required')
+        if len(password) < 8:
+            raise forms.ValidationError('This field must be at least 8 character in length')
+        return password
--- user/views.py
+++ user/views.py
@@ -1,10 +1,28 @@
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
 from .forms import SignupForm
 
 def signup(request):
+    form = SignupForm()
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            user = User.objects.create_user(
+                username=request.POST['username'],
+                email=request.POST['email'],
+                password=request.POST['password'],
+                first_name=request.POST['first_name'],
+                last_name=request.POST['last_name']
+            )
+            user.save()
+            return JsonResponse(request.POST)
+        else:
+            form = SignupForm(request.POST)
+    
     context = {
-        'signup_form': SignupForm(),
+        'signup_form': form,
     }
     return render(request, 'user/signup.html', context)
 
